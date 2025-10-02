d99e0d5ee31c5bb4f6c6043eab4f65a4471e7b5a
Maxim Kleverov <mklevero@student.hive.fi>
2025-10-02 17:00:21 +0300

    start

---
+++ philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/02 16:53:02 by mklevero          #+#    #+#             */
+/*   Updated: 2025/10/02 16:59:30 by mklevero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO
+# define PHILO
+
+# include <limits.h>  // INT_MAX
+# include <pthread.h> // mutex
+# include <stdio.h>   // printf
+# include <stdlib.h>  // malloc free
+# include <sys/time.h>
+# include <unistd.h> // write usleep
+
+#endif
